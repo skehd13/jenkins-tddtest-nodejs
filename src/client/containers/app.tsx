22c8d6f1b6f0e131030dfb568a36f59dea0367ce
dongkyu <skehd133@naver.com>
2022-03-21 18:37:54 +0900

    텍스트 수정

--- src/client/containers/app.tsx
+++ src/client/containers/app.tsx
@@ -38,7 +38,7 @@ const App = () => {
         <input name="email" id="email" />
         <input name="password" id="password" />
         <button type="submit" id="submitBt">
-          완료11
+          완료
         </button>
       </form>
     </div>
